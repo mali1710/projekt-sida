33f0dc7f48305a7d25650d8f685e7daebe852487
Malin Lind <mali1710@student.miun.se>
2019-10-25 15:44:38 +0200

    header med navbar tillagd

---
+++ src/js/navbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+  $("nav ul li").on("click", function() {
+    $("nav ul").removeClass("showing");
+  });
+});
