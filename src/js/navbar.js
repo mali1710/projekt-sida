f1822266396737b5d8ccff6f8d1c5d895806761a
Malin Lind <mali1710@student.miun.se>
2019-10-28 10:25:58 +0100

    design klar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -5,4 +5,12 @@ $(document).ready(function() {
   $("nav ul li").on("click", function() {
     $("nav ul").removeClass("showing");
   });
+  $(document).scroll(function() {
+    var height = 100;
+    if ($(this).scrollTop() > height) {
+      $("nav").css("background-color", "rgba(237, 235, 232, 1)");
+    } else {
+      $("nav").css("background-color", "rgba(237, 235, 232, 0)");
+    }
+  });
 });
